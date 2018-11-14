7f1cf25108f00bca3700c19bd4b991644710a658
GuryevaPolina <36489352+GuryevaPolina@users.noreply.github.com>
2018-11-14 15:08:31 +0300

    add test file

---
+++ test.sh
@@ -0,0 +1,38 @@
+cd /Users/polinaguryeva/Downloads/Daemon-master 
+mkdir test
+cd test
+echo Created file1 and file2: 
+touch file1
+touch file2
+ls
+
+echo sleep 110 sec:
+sleep 110
+echo ls after 110 sec:
+ls
+echo sleep 15 sec:
+sleep 15
+echo ls after 15 sec:
+ls
+
+echo Created file3:
+touch file3
+ls
+
+echo sleep 60 sec:
+sleep 60
+echo ls after 60 sec:
+ls
+echo Created file4:
+touch file4
+ls
+
+echo sleep 65 sec:
+sleep 65
+echo ls after 65 sec:
+ls
+
+echo sleep 65 sec:
+sleep 65
+echo ls after 65 sec:
+ls
